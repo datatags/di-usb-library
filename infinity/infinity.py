59754af96b6a35eed1c4d29740745a53bbf40c83
datatags <datatags@users.noreply.github.com>
2025-10-26 22:18:06 -0700

    Add tag block R/W and detect errors

--- infinity/infinity.py
+++ infinity/infinity.py
@@ -1,17 +1,28 @@
 from collections import defaultdict
+from enum import Enum
 import asyncio
 import hid
 
 DEVICE_VID = 0x0e6f
 DEVICE_PID = 0x0129
 
+
+class ErrorType(Enum):
+    NO_SUCH_TAG = 0x80
+    TAG_IO_ERROR = 0x82
+
+
 class Tag:
-    def __init__(self, index: bytes):
-        self.platform = index[0] >> 4
-        self.index    = index[0] & 0x0F
-        # ISO 14443A SAK, always 0x09 for DI tags
-        self.sak      = index[1]
+    def __init__(self, platform: int, index: int, sak: int):
+        self.platform = platform
+        self.index    = index
+        # ISO 14443A SAK, always 0x09 for DI tags (Mifare Classic Mini)
+        self.sak      = sak
         self.uid      = None
+
+    @staticmethod
+    def from_bytes(index: bytes):
+        return Tag(index[0] >> 4, index[0] & 0x0F, index[1])
 
     def __str__(self):
         return f"Tag(platform={self.platform},index={self.index},sak={self.sak},uid={self.uid})"
@@ -22,9 +33,7 @@ class Tag:
 
 class TagChangeEvent:
     def __init__(self, data: bytes):
-        self.platform = data[2]
-        self.tag_type = data[3]
-        self.tag_index = data[4]
+        self.tag = Tag(data[2], data[4], data[3])
         self.is_removed = bool(data[5])
 
 
@@ -132,7 +141,10 @@ class InfinityBase(object):
             return {}
         tagByPlatform = defaultdict(list)
         for tag in tags:
-            await self.load_tag_uid(tag)
+            try:
+                await self.load_tag_uid(tag)
+            except ValueError as e:
+                print(e)
             tagByPlatform[tag.platform].append(tag)
         return dict(tagByPlatform)
 
@@ -140,12 +152,15 @@ class InfinityBase(object):
         data = await self.comms.send_message(0xa1)
         tags = []
         for i in range(0, len(data), 2):
-            tags.append(Tag(data[i:i+2]))
+            tags.append(Tag.from_bytes(data[i:i+2]))
         return tags
 
     async def load_tag_uid(self, tag: Tag):
+        data = await self.comms.send_message(0xb4, [tag.index])
         # First byte is a status or something, 0x00 if the tag exists, 0x80 if it doesn't
-        tag.uid = (await self.comms.send_message(0xb4, [tag.index]))[1:]
+        if data[0] == ErrorType.NO_SUCH_TAG.value:
+            raise ValueError("No such tag")
+        tag.uid = data[1:]
 
     async def set_color(self, platform: int, r: int, g: int, b: int):
         await self.comms.send_message(0x90, [platform, r, g, b])
@@ -156,20 +171,53 @@ class InfinityBase(object):
     async def flash_color(self, platform: int, r: int, g: int, b: int):
         await self.comms.send_message(0x93, [platform, 0x02, 0x02, 0x06, r, g, b])
 
+    async def read_tag(self, tag: Tag, sector: int, offset: int = 0) -> bytes:
+        """Read a data block from the tag.
+
+        The actual block read is `(sector * 4) + offset`, and there don't
+        appear to be any artificial limits on the parameters, e.g. using
+        sector=0 offset=14 works just as well as sector=3 offset=2
+
+        Keyword arguments:
+        tag -- the tag to read from
+        sector -- the sector to read from
+        offset -- the offset within the sector to read
+        """
+        data = await self.comms.send_message(0xa2, [tag.index, sector, offset])
+        if data[0] == ErrorType.TAG_IO_ERROR.value:
+            raise ValueError("Tag read error")
+        return data[1:]
+
+    async def write_tag(self, tag: Tag, sector: int, data: bytes, offset: int = 0):
+        """Write a data block to the tag. See `read_tag` for more info."""
+        if len(data) != 16:
+            raise ValueError("Must supply exactly 16 bytes")
+        data = await self.comms.send_message(0xa3, [tag.index, sector, offset] + list(data))
+        if data[0] == ErrorType.TAG_IO_ERROR.value:
+            raise ValueError("Tag write error")
+
+
 async def main():
     base = InfinityBase()
 
     async def on_change(event: TagChangeEvent):
+        if not event.is_removed:
+            try:
+                data = await base.read_tag(event.tag, 0)
+                print(f"Tag data, block 0: {data.hex()}")
+            except ValueError as e:
+                print(f"Failed to read tag data: {e}")
+
         tags = await base.get_all_tags()
         color = (0, 0, 0)
-        count = len(tags.get(event.platform, []))
+        count = len(tags.get(event.tag.platform, []))
         if count == 1:
             color = (0, 0, 200)
         elif count == 2:
             color = (0, 56, 0)
         elif count > 2:
             color = (200, 0, 0)
-        await base.set_color(event.platform, *color)
+        await base.set_color(event.tag.platform, *color)
 
     base.on_tags_changed = on_change
 
